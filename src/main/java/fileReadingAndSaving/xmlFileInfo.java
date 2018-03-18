95acdca0d75fcdeeb277c86fa5c90daa920db839
Eyepatchrabbit <boffin.alexander@gmail.com>
2018-03-18 17:19:11 +0100

    adding the file structure for the project

---
+++ src/main/java/fileReadingAndSaving/xmlFileInfo.java
@@ -0,0 +1,8 @@
+package fileReadingAndSaving;
+
+public class xmlFileInfo {
+
+    public static void main(String[] args) {
+        //todo; create here the methods and means to get info from the files (csv-files)
+    }
+}
---
+++ src/test/java/fileReadingAndSaving/testXmlFileInfo.java
@@ -0,0 +1,7 @@
+package fileReadingAndSaving;
+
+public class testXmlFileInfo {
+    public static void main(String[] args) {
+        //for testing the methods needed to test the javaclass-elements
+    }
+}
